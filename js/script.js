4cde3446d795df0e7a4d2bc3edd06d5999ba248e
Wrecker <57878086+TheLocalFurry@users.noreply.github.com>
2023-04-28 19:33:28 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,116 +1,125 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
-import {OrbitControls} from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls";
+//Get window size
+var ww = window.innerWidth,
+  wh = window.innerHeight;
 
-console.clear();
+//Create a WebGL renderer
+var renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("canvas")
+});
+renderer.setSize(ww, wh);
 
-let scene = new THREE.Scene();
-scene.background = new THREE.Color(0x160016);
-let camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 1, 1000);
-camera.position.set(0, 4, 21);
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement);
-window.addEventListener("resize", event => {
-  camera.aspect = innerWidth / innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(innerWidth, innerHeight);
-})
+//Create an empty scene
+var scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0x000000, 30, 150);
 
-let controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.enablePan = false;
+//Create a perpsective camera
+var camera = new THREE.PerspectiveCamera(45, ww / wh, 0.1, 150);
+camera.position.y = 400;
+camera.position.z = 400;
 
-let gu = {
-  time: {value: 0}
+//Array of points
+var points = [
+  [68.5, 185.5],
+  [1, 262.5],
+  [270.9, 281.9],
+  [345.5, 212.8],
+  [178, 155.7],
+  [240.3, 72.3],
+  [153.4, 0.6],
+  [52.6, 53.3],
+  [68.5, 185.5]
+];
+
+//Convert the array of points into vertices
+for (var i = 0; i < points.length; i++) {
+  var x = points[i][0];
+  var y = Math.random() * 100;
+  var z = points[i][1];
+  points[i] = new THREE.Vector3(x, y, z);
 }
+//Create a path from the points
+var path = new THREE.CatmullRomCurve3(points);
+path.closed = true;
 
-let sizes = [];
-let shift = [];
-let pushShift = () => {
-  shift.push(
-    Math.random() * Math.PI, 
-    Math.random() * Math.PI * 2, 
-    (Math.random() * 0.9 + 0.1) * Math.PI * 0.1,
-    Math.random() * 0.9 + 0.1
-  );
-}
-let pts = new Array(50000).fill().map(p => {
-  sizes.push(Math.random() * 1.5 + 0.5);
-  pushShift();
-  return new THREE.Vector3().randomDirection().multiplyScalar(Math.random() * 0.5 + 9.5);
-})
-for(let i = 0; i < 100000; i++){
-  let r = 10, R = 40;
-  let rand = Math.pow(Math.random(), 1.5);
-  let radius = Math.sqrt(R * R * rand + (1 - rand) * r * r);
-  pts.push(new THREE.Vector3().setFromCylindricalCoords(radius, Math.random() * 2 * Math.PI, (Math.random() - 0.5) * 2 ));
-  sizes.push(Math.random() * 1.5 + 0.5);
-  pushShift();
-}
+// Define the precision of the finale tube, the amount of divisions
+var tubeDetail = 1600;
+// Define the precision of the circles
+var circlesDetail = 40;
 
-let g = new THREE.BufferGeometry().setFromPoints(pts);
-g.setAttribute("sizes", new THREE.Float32BufferAttribute(sizes, 1));
-g.setAttribute("shift", new THREE.Float32BufferAttribute(shift, 4));
-let m = new THREE.PointsMaterial({
-  size: 0.125,
-  transparent: true,
-  depthTest: false,
-  blending: THREE.AdditiveBlending,
-  onBeforeCompile: shader => {
-    shader.uniforms.time = gu.time;
-    shader.vertexShader = `
-      uniform float time;
-      attribute float sizes;
-      attribute vec4 shift;
-      varying vec3 vColor;
-      ${shader.vertexShader}
-    `.replace(
-      `gl_PointSize = size;`,
-      `gl_PointSize = size * sizes;`
-    ).replace(
-      `#include <color_vertex>`,
-      `#include <color_vertex>
-        float d = length(abs(position) / vec3(40., 10., 40));
-        d = clamp(d, 0., 1.);
-        vColor = mix(vec3(227., 155., 0.), vec3(100., 50., 255.), d) / 255.;
-      `
-    ).replace(
-      `#include <begin_vertex>`,
-      `#include <begin_vertex>
-        float t = time;
-        float moveT = mod(shift.x + shift.z * t, PI2);
-        float moveS = mod(shift.y + shift.z * t, PI2);
-        transformed += vec3(cos(moveS) * sin(moveT), cos(moveT), sin(moveS) * sin(moveT)) * shift.w;
-      `
-    );
-    //console.log(shader.vertexShader);
-    shader.fragmentShader = `
-      varying vec3 vColor;
-      ${shader.fragmentShader}
-    `.replace(
-      `#include <clipping_planes_fragment>`,
-      `#include <clipping_planes_fragment>
-        float d = length(gl_PointCoord.xy - 0.5);
-        //if (d > 0.5) discard;
-      `
-    ).replace(
-      `vec4 diffuseColor = vec4( diffuse, opacity );`,
-      `vec4 diffuseColor = vec4( vColor, smoothstep(0.5, 0.1, d)/* * 0.5 + 0.5*/ );`
-    );
-    //console.log(shader.fragmentShader);
+// Define the radius of the finale tube
+var radius = 4;
+// Get all the circles that will compose the tube
+var frames = path.computeFrenetFrames(tubeDetail, true);
+
+// Create an empty Geometry where we will put the particles
+var geometry = new THREE.Geometry();
+
+// Define a basic color
+var color = new THREE.Color(0x000000);
+
+// First loop through all the circles
+for (var i = 0; i < tubeDetail; i++) {
+  // Get the normal values for each circle
+  var normal = frames.normals[i];
+  // Get the binormal values
+  var binormal = frames.binormals[i];
+
+  // Calculate the index of the circle (from 0 to 1)
+  var index = i / tubeDetail;
+  // Get the coordinates of the point in the center of the circle
+  var p = path.getPointAt(index);
+
+  // Loop for the amount of particles we want along each circle
+  for (var j = 0; j < circlesDetail; j++) {
+    // Clone the position of the point in the center
+    var position = p.clone();
+
+    // Calculate the angle for each particle along the circle (from 0 to Pi*2)
+    var angle = (j / circlesDetail) * Math.PI * 2 + (index * Math.PI * 5);
+    // Calculate the sine of the angle
+    var sin = Math.sin(angle);
+    // Calculate the cosine from the angle
+    var cos = -Math.cos(angle);
+
+    // Calculate the normal of each point based on its angle
+    var normalPoint = new THREE.Vector3(0, 0, 0);
+    normalPoint.x = (cos * normal.x + sin * binormal.x);
+    normalPoint.y = (cos * normal.y + sin * binormal.y);
+    normalPoint.z = (cos * normal.z + sin * binormal.z);
+    // Multiple the normal by the radius
+    normalPoint.multiplyScalar(radius);
+
+    // We add the normal values for each point
+    position.add(normalPoint);
+    var color = new THREE.Color("hsl(" + (index * 360 * 4) + ", 100%, 50%)");
+    geometry.colors.push(color);
+    geometry.vertices.push(position);
   }
+}
+
+// Material for the points
+var material = new THREE.PointsMaterial({
+  size: 0.2,
+  vertexColors: THREE.VertexColors
 });
-let p = new THREE.Points(g, m);
-p.rotation.order = "ZYX";
-p.rotation.z = 0.2;
-scene.add(p)
 
-let clock = new THREE.Clock();
+var tube = new THREE.Points(geometry, material);
+//Add tube into the scene
+scene.add(tube);
 
-renderer.setAnimationLoop(() => {
-  controls.update();
-  let t = clock.getElapsedTime() * 0.5;
-  gu.time.value = t * Math.PI;
-  p.rotation.y = t * 0.05;
+var percentage = 0;
+
+function render() {
+
+  percentage += 0.0005;
+  var p1 = path.getPointAt(percentage % 1);
+  var p2 = path.getPointAt((percentage + 0.01) % 1);
+  camera.position.set(p1.x, p1.y, p1.z);
+  camera.lookAt(p2);
+
+  //Render the scene
   renderer.render(scene, camera);
-});
+
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
